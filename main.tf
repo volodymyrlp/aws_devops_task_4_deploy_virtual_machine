25120883b04fa478100fcb58162aeeb187cb3cf6
Lysenko Volodymyr <mr.volodumyr@gmail.com>
2026-06-26 23:27:34 +0200

    Solution

--- main.tf
+++ main.tf
@@ -1,28 +1,43 @@
-# Uncomment the ami datasource to get the AMI id for your ec2 instance: 
-# data "aws_ami" "this" {
-#   most_recent = true
-#   owners      = ["amazon"]
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
 
-#   filter {
-#     name = "name"
-#     values = ["amzn2-ami-hvm-*-gp2"]
-#   }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-gp2"]
+  }
 
-#   filter {
-#     name   = "root-device-type"
-#     values = ["ebs"]
-#   }
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   filter {
-#     name   = "architecture"
-#     values = ["x86_64"]
-#   }
-# }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
 
-# create a key pair 
-# create an ec2 instance 
+resource "aws_key_pair" "this" {
+  key_name   = "mate-aws-grafana-lab"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "this" {
+  ami                         = data.aws_ami.this.id
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  key_name                    = aws_key_pair.this.key_name
+  user_data                   = file("${path.module}/install-grafana.sh")
+
+  tags = {
+    Name = "mate-aws-grafana-lab"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "instance_public_ip" {
-    value = aws_instance.this.public_ip
-    sensitive = false
+  value     = aws_instance.this.public_ip
+  sensitive = false
 }
 
 output "grafana_url" {
-    value = "http://${aws_instance.this.public_ip}:3000/"
-    sensitive = false
+  value     = "http://${aws_instance.this.public_ip}:3000/"
+  sensitive = false
 }
